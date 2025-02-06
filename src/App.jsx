c474b35a8b4ed95dac2fd76bef8ad5900e483d6e
Stanley Meruo <144041200+stanley-meruo@users.noreply.github.com>
2025-02-06 03:14:58 +0100

    FIX: created a .env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,3 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-// } from "react-router-dom";
-// import { useState, useEffect } from "react";
-// import { supabase } from "/supabase";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Dashboard from "./pages/Dashboard";
-
-// function App() {
-//   const [user, setUser] = useState(null);
-
-//   useEffect(() => {
-//     const getUser = async () => {
-//       const {
-//         data: { user },
-//       } = await supabase.auth.getUser();
-//       setUser(user);
-//     };
-//     getUser();
-
-//     supabase.auth.onAuthStateChange((_event, session) => {
-//       setUser(session?.user || null);
-//     });
-//   }, []);
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={user ? <Navigate to="/dashboard" /> : <Home />}
-//         />
-//         <Route
-//           path="/login"
-//           element={user ? <Navigate to="/dashboard" /> : <Login />}
-//         />
-//         <Route
-//           path="/dashboard"
-//           element={user ? <Dashboard user={user} /> : <Navigate to="/login" />}
-//         />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import {
   BrowserRouter as Router,
   Routes,
@@ -74,6 +22,5 @@ function App() {
     </AuthProvider>
   );
 }
-
 export default App;
 
